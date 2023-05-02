d0bd4f22e9efd30b58e802f8fc0e761d178ec487
TarasKarabas <taraskyparenko@gmail.com>
2023-05-02 18:24:49 +1030

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Taras Kyparenko on 2/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
